86089241b7e8e7d79cfd8c85a135472abac522bd
eminencesrec <eminence@srecnandyal.edu.in>
2025-02-07 18:40:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scroll Functionality (if needed)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
